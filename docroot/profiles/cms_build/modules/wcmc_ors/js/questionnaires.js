6dfa42509fc2c71a5189faabe81aff6b7d06ef7d
Cristian Romanescu <cristian@romanescu.ro>
2016-06-10 12:47:35 +0300

    Added module configuration screen

--- docroot/profiles/cms_build/modules/wcmc_ors/js/questionnaires.js
+++ docroot/profiles/cms_build/modules/wcmc_ors/js/questionnaires.js
@@ -1,7 +1,6 @@
 (function($) {
   google.charts.load("current", {packages:["corechart"]});
   $.fn.drawChartQuestionnaire = function(data) {
-    console.log(data);
     if (data.length > 0) {
       var chart = new google.visualization.PieChart(document.getElementById('respondents-chart'));
       var options = {};
