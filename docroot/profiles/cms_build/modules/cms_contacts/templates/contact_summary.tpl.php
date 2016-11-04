3e179e1ac8f8355d17777b6a2bc8c09922670d77
Claudia Ifrim <ifrim.claudia@gmail.com>
2016-11-04 14:55:48 +0200

    update summary field per contact

--- docroot/profiles/cms_build/modules/cms_contacts/templates/contact_summary.tpl.php
+++ docroot/profiles/cms_build/modules/cms_contacts/templates/contact_summary.tpl.php
@@ -1,10 +1,23 @@
 <div class="form-group">
     <textarea class="form-control" rows="10"><?php
+        if (!empty($contact->clean_contact['personaltitle'])) {
+            echo $contact->clean_contact['personaltitle'] . " " . $contact->first_name . " " . $contact->last_name . "\n";
+        } else {
+            echo $contact->first_name . " " . $contact->last_name . " \n";
+        }
+        if (!empty($contact->clean_contact['title'])) {
+            echo $contact->clean_contact['title'] . "\n";
+        }
+        if (!empty($contact->department)) {
+            echo $contact->department . "\n";
+        }
         echo
-            $contact->first_name . " " . $contact->last_name . " \n" .
             $contact->organization . " \n" .
-            $contact->clean_contact['street'] . " \n" .
-            $contact->clean_contact['st'] . ", " . $contact->country_rendered . " \n";
+            $contact->clean_contact['street'] . " \n";
+        if (!empty($contact->clean_contact['postalcode'])) {
+            echo $contact->clean_contact['postalcode'] . "\n";
+        }
+        echo $contact->clean_contact['st'] . ", " . $contact->country_rendered . " \n";
         if (!empty($contact->clean_contact['mail'])) {
             echo t('E-mail') . ': ' . $contact->clean_contact['mail'] . " \n";
         }
